84d512909f28dd773a314ea3b2939f534e964822
bianca <bianca.ianc@3pillarglobal.com>
2021-08-23 15:38:55 +0300

    Initial commit: create a spring-boot project.

---
+++ src/main/java/com/internship/pillarglobal/NewsfeedMapperAPI/NewsfeedMapperApiApplication.java
@@ -0,0 +1,13 @@
+package com.internship.pillarglobal.NewsfeedMapperAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsfeedMapperApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsfeedMapperApiApplication.class, args);
+	}
+
+}
