5492c1aac3b5086ce18ce20cc4b88deadcf3a3ef
bianca <bianca.ianc@3pillarglobal.com>
2021-08-26 22:41:13 +0300

    Save YahooUKItems to DB-3.

--- src/main/java/com/internship/pillarglobal/NewsfeedMapperAPI/models/YahooUKItem.java
+++ src/main/java/com/internship/pillarglobal/NewsfeedMapperAPI/models/YahooUKItem.java
@@ -2,6 +2,9 @@ package com.internship.pillarglobal.NewsfeedMapperAPI.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.w3c.dom.Text;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -11,16 +14,24 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name="yahoo_uk_items",schema = "newsfeed_api_db_schema")
-@Data
+
 
 public class YahooUKItem {
+    @Column(name = "title")
     private String title;
+    @Column(name = "link")
     private String link;
+    @Column(name = "pubDate")
     private Date pubDate;
+    @Column(name = "premium")
     private boolean premium;
     @Id
+    @Column(name = "guid")
+
     private String guid;
+    @Column(name = "dc_creator")
     private String dc_creator;
+    @Column(length = 300000)
     private String content_encoded;
     private String description;
     private String category;
